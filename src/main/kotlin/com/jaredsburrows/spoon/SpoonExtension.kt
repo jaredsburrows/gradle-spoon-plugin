15fca2f53a138fae3069c60b8752b74d140d10ff
Jared Burrows <jaredsburrows@gmail.com>
2017-11-13 20:05:08 -0800

    move 'SpoonTask' to Kotlin

--- src/main/kotlin/com/jaredsburrows/spoon/SpoonExtension.kt
+++ src/main/kotlin/com/jaredsburrows/spoon/SpoonExtension.kt
@@ -12,100 +12,100 @@ import java.io.File
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  */
 open class SpoonExtension { // Extensions cannot be final
-  companion object {
-    private const val DEFAULT_OUTPUT_DIRECTORY = "spoon-output"
-    private const val DEFAULT_ADB_TIMEOUT_SEC = 10 * 60  // 10 minutes
-  }
-
-  ////////////////////////////////////////////////////
-  // Supported directly by Spoon's SpoonRunner
-  ///////////////////////////////////////////////////
-
-  /** Path to output directory. ("$buildDir/spoon-output" by default) */
-  var output: String = DEFAULT_OUTPUT_DIRECTORY
-    set(value) {
-      field = value
+    companion object {
+        private const val DEFAULT_OUTPUT_DIRECTORY = "spoon-output"
+        private const val DEFAULT_ADB_TIMEOUT_SEC = 10 * 60  // 10 minutes
     }
 
-  /** Whether or not debug logging is enabled. (false by default) */
-  var debug: Boolean = false
+    ////////////////////////////////////////////////////
+    // Supported directly by Spoon's SpoonRunner
+    ///////////////////////////////////////////////////
 
-  /** Whether or not animations are enabled. Disable animated gif generation. (false by default) */
-  var noAnimations: Boolean = false
+    /** Path to output directory. ("$buildDir/spoon-output" by default) */
+    var output: String = DEFAULT_OUTPUT_DIRECTORY
+        set(value) {
+            field = value
+        }
 
-  /** Set ADB timeout. (minutes) (default is 10 minutes) */
-  var adbTimeout: Int = DEFAULT_ADB_TIMEOUT_SEC * 1000
-    set(value) {
-      if (value > 0) field = value * 1000
-    }
+    /** Whether or not debug logging is enabled. (false by default) */
+    var debug: Boolean = false
 
-  /** Add device serials for test execution. */
-  var devices: Set<String> = emptySet()
+    /** Whether or not animations are enabled. Disable animated gif generation. (false by default) */
+    var noAnimations: Boolean = false
 
-  /** Add device serials for skipping test execution. */
-  var skipDevices: Set<String> = emptySet()
+    /** Set ADB timeout. (minutes) (default is 10 minutes) */
+    var adbTimeout: Int = DEFAULT_ADB_TIMEOUT_SEC * 1000
+        set(value) {
+            if (value > 0) field = value * 1000
+        }
 
-  /** Extra arguments to pass to instrumentation. */
-  var instrumentationArgs: List<String> = emptyList()
+    /** Add device serials for test execution. */
+    var devices = mutableSetOf<String>()
 
-  /** Test class name to run (fully-qualified). */
-  var className: String = ""
+    /** Add device serials for skipping test execution. */
+    var skipDevices = mutableSetOf<String>()
 
-  // TODO size
+    /** Extra arguments to pass to instrumentation. */
+    var instrumentationArgs = mutableListOf<String>()
 
-  /** Execute the tests device by device. (false by default) */
-  var sequential: Boolean = false
+    /** Test class name to run (fully-qualified). */
+    var className: String = ""
 
-  /** Grant all runtime permissions during installation on Marshmallow and above devices. (false by default) */
-  var grantAll: Boolean = false
+    // TODO size
 
-  /** Test method name to run (must also use className) */
-  var methodName: String = ""
+    /** Execute the tests device by device. (false by default) */
+    var sequential: Boolean = false
 
-  /** Code coverage flag. For Spoon to calculate coverage file your app must have the `WRITE_EXTERNAL_STORAGE` permission. (false by default)
-  (This option pulls the coverage file from all devices and merge them into a single file `merged-coverage.ec`.) */
-  var codeCoverage: Boolean = false
+    /** Grant all runtime permissions during installation on Marshmallow and above devices. (false by default) */
+    var grantAll: Boolean = false
 
-  /** Fail if no device is connected. (false by default) */
-  var failIfNoDeviceConnected: Boolean = false
+    /** Test method name to run (must also use className) */
+    var methodName: String = ""
 
-  ////////////////////////////////////////////////////
-  // Passed in via -e, extra arguments
-  ///////////////////////////////////////////////////
+    /** Code coverage flag. For Spoon to calculate coverage file your app must have the `WRITE_EXTERNAL_STORAGE` permission. (false by default)
+    (This option pulls the coverage file from all devices and merge them into a single file `merged-coverage.ec`.) */
+    var codeCoverage: Boolean = false
 
-  /** Toggle sharding. (false by default) */
-  var shard: Boolean = false
+    /** Fail if no device is connected. (false by default) */
+    var failIfNoDeviceConnected: Boolean = false
 
-  /** The number of separate shards to create. */
-  var numShards: Int = 0
-    set(value) {
-      if (value > 0) field = value
-    }
+    ////////////////////////////////////////////////////
+    // Passed in via -e, extra arguments
+    ///////////////////////////////////////////////////
 
-  /** The shardIndex option to specify which shard to run. */
-  var shardIndex: Int = 0
-    set(value) {
-      if (value > 0) field = value
-    }
+    /** Toggle sharding. (false by default) */
+    var shard: Boolean = false
 
-  /** Do not fail build if a test fails, let all the tests run and finish. (false by default) */
-  var ignoreFailures: Boolean = false
+    /** The number of separate shards to create. */
+    var numShards: Int = 0
+        set(value) {
+            if (value > 0) field = value
+        }
 
-  ////////////////////////////////////////////////////
-  // Deprecated/Renamed
-  ///////////////////////////////////////////////////
+    /** The shardIndex option to specify which shard to run. */
+    var shardIndex: Int = 0
+        set(value) {
+            if (value > 0) field = value
+        }
 
-  @Deprecated("Use 'output'", replaceWith = ReplaceWith("output"))
-  var baseOutputDir: File = File(DEFAULT_OUTPUT_DIRECTORY)
-    set(value) {
-      output = value.absolutePath
-      field = value
-    }
+    /** Do not fail build if a test fails, let all the tests run and finish. (false by default) */
+    var ignoreFailures: Boolean = false
 
-  @Deprecated("Use 'grantAll'", replaceWith = ReplaceWith("grantAll"))
-  var grantAllPermissions: Boolean = false
-    set(value) {
-      grantAll = value
-      field = value
-    }
+    ////////////////////////////////////////////////////
+    // Deprecated/Renamed
+    ///////////////////////////////////////////////////
+
+    @Deprecated("Use 'output'", replaceWith = ReplaceWith("output"))
+    var baseOutputDir: File = File(DEFAULT_OUTPUT_DIRECTORY)
+        set(value) {
+            output = value.absolutePath
+            field = value
+        }
+
+    @Deprecated("Use 'grantAll'", replaceWith = ReplaceWith("grantAll"))
+    var grantAllPermissions: Boolean = false
+        set(value) {
+            grantAll = value
+            field = value
+        }
 }
---
+++ src/main/kotlin/com/jaredsburrows/spoon/SpoonTask.kt
@@ -0,0 +1,106 @@
+package com.jaredsburrows.spoon
+
+import com.squareup.spoon.SpoonRunner
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+import java.time.Duration
+
+/**
+ * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
+ */
+open class SpoonTask : DefaultTask() {
+    /** Title of the generated HTML website. */
+    lateinit var title: String
+
+    /** Use our Spoon extension. */
+    lateinit var extension: SpoonExtension
+
+    /** Application APK (eg. app-debug.apk). */
+    lateinit var applicationApk: File
+
+    /** Instrumentation APK (eg. app-debug-androidTest.apk). */
+    lateinit var instrumentationApk: File
+
+    /** TESTING ONLY */
+    var testing: Boolean = false
+
+    @Suppress("unused")
+    @TaskAction
+    fun spoonTask() {
+        if (extension.className.isNotEmpty() && extension.methodName.isEmpty()) {
+            throw IllegalStateException("$extension.methodName must have a fully qualified class name.")
+        }
+
+        val builder = SpoonRunner.Builder()
+            // Not in extension
+            .setTerminateAdb(false) // default is true
+            .setTitle(title)
+            .addOtherApk(applicationApk)
+            .setTestApk(instrumentationApk)
+            // In SpoonExtension
+            .setOutputDirectory(File(extension.output))
+            .setDebug(extension.debug)
+            .setNoAnimations(extension.noAnimations)
+            .setAdbTimeout(Duration.ofSeconds(extension.adbTimeout.toLong()))
+            .setClassName(extension.className)
+            .setSequential(extension.sequential)
+            .setGrantAll(extension.grantAll)
+            .setMethodName(extension.methodName)
+            .setCodeCoverage(extension.codeCoverage)
+            .setAllowNoDevices(!extension.failIfNoDeviceConnected)
+            .setShard(extension.shard)
+
+        // File and add the SDK
+        val android = project.extensions.findByName("android")
+        val sdkDirectory = android?.javaClass?.getMethod("getSdkDirectory")?.invoke(android) as File?
+        if (sdkDirectory != null) {
+            builder.setAndroidSdk(sdkDirectory)
+        }
+
+        // Add shard information to instrumentation args if there are any
+        if (extension.numShards > 0) {
+            extension.instrumentationArgs.add("numShards=${extension.numShards}")
+            extension.instrumentationArgs.add("shardIndex=${extension.shardIndex}")
+        }
+
+        // If we have args apply them else let them be null
+        if (extension.instrumentationArgs.isNotEmpty()) {
+            builder.setInstrumentationArgs(extension.instrumentationArgs)
+        }
+
+        // Add all skipped devices
+        extension.skipDevices.forEach {
+            builder.skipDevice(it)
+        }
+
+        // Add all devices
+        extension.devices.forEach {
+            builder.addDevice(it)
+        }
+
+        logger.log(LogLevel.INFO, "Run instrumentation tests $instrumentationApk for app $applicationApk")
+        logger.log(LogLevel.INFO, "Output: $extension.output")
+        logger.log(LogLevel.INFO, "Ignore failures: $extension.ignoreFailures")
+        logger.log(LogLevel.INFO, "Fail if no device connected: $extension.failIfNoDeviceConnected")
+        logger.log(LogLevel.INFO, "Debug mode: $extension.debug")
+        if (extension.className.isNotEmpty()) {
+            logger.log(LogLevel.INFO, "Class name: $extension.className")
+            if (extension.methodName.isNotEmpty()) {
+                logger.log(LogLevel.INFO, "Method name: $extension.methodName")
+            }
+        }
+        logger.log(LogLevel.INFO, "No animations: $extension.noAnimations")
+        logger.log(LogLevel.INFO, "numShards: $extension.numShards")
+        logger.log(LogLevel.INFO, "shardIndex: $extension.shardIndex")
+
+        val runner = builder.build()
+        val success = if (testing) true else runner.run()
+
+        if (!success && !extension.ignoreFailures) {
+            throw GradleException("Tests failed! See ${extension.output}/index.html")
+        }
+    }
+}
