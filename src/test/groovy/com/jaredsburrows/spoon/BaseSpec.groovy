6ddaec4bd17d238d62f39b4d804142c40a53ac3b
Jared Burrows <jaredsburrows@gmail.com>
2017-11-23 10:53:19 -0500

    use android sdk

--- src/test/groovy/com/jaredsburrows/spoon/BaseSpec.groovy
+++ src/test/groovy/com/jaredsburrows/spoon/BaseSpec.groovy
@@ -1,6 +1,5 @@
 package com.jaredsburrows.spoon
 
-import com.android.build.gradle.internal.SdkHandler
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
@@ -12,10 +11,9 @@ import spock.lang.Specification
  */
 abstract class BaseSpec extends Specification {
   @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
-  static def COMPILE_SDK_VERSION = 26
-  static def BUILD_TOOLS_VERSION = "26.0.2"
+  static def COMPILE_SDK_VERSION = 27
+  static def BUILD_TOOLS_VERSION = "27.0.1"
   static def APPLICATION_ID = "com.example"
-  static def TEST_ANDROID_SDK = this.getResource("/android-sdk").toURI() // Test fixture that emulates a local android sdk
   static def MANIFEST_FILE_PATH = "src/main/AndroidManifest.xml"
   static def MANIFEST = "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"$APPLICATION_ID\"/>"
   static def APP_APK = "project-debug.apk"
@@ -39,8 +37,5 @@ abstract class BaseSpec extends Specification {
     testProjectDir.newFolder("build", "outputs", "apk", "debug")
     appApk = testProjectDir.newFile("build/outputs/apk/debug/" + APP_APK)
     testApk = testProjectDir.newFile("build/outputs/apk/debug/" + TEST_APK)
-
-    // Set mock test sdk, we only need to test the plugins tasks
-    SdkHandler.sTestSdkFolder = project.file(TEST_ANDROID_SDK)
   }
 }
