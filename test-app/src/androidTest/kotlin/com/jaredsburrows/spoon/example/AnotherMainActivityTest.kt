1bdad893512141050315825b6681dcbb4fbbfb14
Jared Burrows <jaredsburrows@gmail.com>
2019-09-08 13:13:52 -0700

    update project

--- test-app/src/androidTest/kotlin/com/jaredsburrows/spoon/example/AnotherMainActivityTest.kt
+++ test-app/src/androidTest/kotlin/com/jaredsburrows/spoon/example/AnotherMainActivityTest.kt
@@ -4,24 +4,19 @@ import android.support.test.annotation.UiThreadTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.widget.TextView
-
-import com.squareup.spoon.SpoonRule
-import com.jaredsburrows.spoon.example.MainActivity
-
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import com.google.common.truth.Truth.assertThat
+import com.jaredsburrows.spoon.example.MainActivity
+import com.squareup.spoon.SpoonRule
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.Test
 
 @RunWith(AndroidJUnit4::class)
 class AnotherMainActivityTest {
   @get:Rule val spoon = SpoonRule()
   @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
 
-  @Test
-  @UiThreadTest
-  fun testSetText() {
+  @Test @UiThreadTest fun testSetText() {
     val act = activityRule.getActivity()
     val text = act.findViewById(android.R.id.text1) as TextView
     spoon.screenshot(act, "startup")
